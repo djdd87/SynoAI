2683dfb75dafbc4d605bb1ad4d5d06a6acdaf9b3
djdd87 <dan@djdd.co.uk>
2021-01-26 10:18:59 +0000

    Moved project to add unit tests

---
+++ SynoAI.Tests/NotificationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SynoAI.Tests
+{
+    public class NotificationTests
+    {
+        [Fact]
+        public void Webhook_ShouldSendBasicAuthentication()
+        {
+
+        }
+    }
+}
