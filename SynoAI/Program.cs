142befafd488abf628c6ec83a68391e56001a424
Sander de Wit <sander@dewit-automatisering.nl>
2023-09-10 18:41:47 +0200

    layer 8 issue with timestamp, all resolved :-)

--- SynoAI/Program.cs
+++ SynoAI/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging;
-
 namespace SynoAI
 {
     /// <summary>
@@ -13,26 +11,14 @@ namespace SynoAI
         /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
-            var loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder.AddProvider(new TimestampLoggerProvider());
-            });
-
-            CreateHostBuilder(args)
-                .ConfigureLogging((context, logging) =>
-                {
-                    logging.ClearProviders(); // Remove any default log providers
-                    logging.AddConsole();    // Add the console logger
-                })
-                .Build()
-                .Run();
-
+            CreateHostBuilder(args).Build().Run();
         }
         /// <summary>
         /// Main method to create and configure the host for the SynoAI application.
         /// </summary>
         /// <param name="args">Command-line arguments.</param>
         /// <returns>An <see cref="IHostBuilder"/> that configures the host for the application.</returns>
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
