df5031501780be810fb106cf0d592f6fcc8bdd4b
Dan Done <daniel.done@point74.co.uk>
2024-09-13 20:03:25 +0100

    Adding templating project.

---
+++ SynoAI.Core.Templating/IMessageTemplateGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace SynoAI.Core.Templating
+{
+    public interface IMessageTemplateGenerator
+    {
+        Task<string> ProcessTemplateAsync(object data, string template);
+    }
+}
---
+++ SynoAI.Core.Templating/ScribanTemplateGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynoAI.Core.Templating
+{
+    public class ScribanTemplateGenerator : IMessageTemplateGenerator
+    {
+        public Task<string> ProcessTemplateAsync(object data, string template)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
